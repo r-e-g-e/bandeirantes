a0d1cf890d61ed4e655d5fd4d7e895c51d9820ed
Rodrigo Tavares <rodrigo.tavares.lima@hotmail.com>
2024-02-27 02:07:07 -0300

    feat: basic http server

--- apps/matchmaker/main.go
+++ apps/matchmaker/main.go
@@ -1,12 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
-func Hello(name string) string {
-	result := "Hello " + name
-	return result
+func methodValidator(writer http.ResponseWriter, request *http.Request) {
+	allowedMethods := []string{"GET", "POST"}
+
+	for i := 0; i < len(allowedMethods); i++ {
+		var result bool = strings.ToLower(allowedMethods[i]) != strings.ToLower(request.Method)
+
+	}
+
 }
 
 func main() {
-	fmt.Println(Hello("matchmaker"))
+	http.HandleFunc("/hello", func(writer http.ResponseWriter, request *http.Request) {
+		something, err := request.Body.Read()
+		log.Println(something)
+		fmt.Fprintf(writer, "Hello!")
+	})
+
+	port := ":3010"
+	fmt.Println("HTTP server listening on port", port)
+
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
